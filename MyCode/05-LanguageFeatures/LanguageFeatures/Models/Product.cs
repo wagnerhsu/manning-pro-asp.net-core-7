017455beaf122c5da15cf028e96e284c7001b41f
Wagner Xu <wagnerhsu@hotmail.com>
2024-10-06 19:06:58 +0800

    feat: add 05-LanguageFeatures

---
+++ MyCode/05-LanguageFeatures/LanguageFeatures/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿namespace LanguageFeatures.Models
+{
+    public class Product
+    {
+
+        public required string Name { get; set; } = string.Empty;
+        public decimal? Price { get; set; }
+
+        public bool NameBeginsWithS => Name.Length > 0 && Name[0] == 'S';
+
+        public static Product?[] GetProducts()
+        {
+
+            Product kayak = new Product
+            {
+                Name = "Kayak",
+                Price = 275M
+            };
+
+            Product lifejacket = new Product
+            {
+                Name = "Lifejacket", 
+                Price = 48.95M
+            };
+
+            return [kayak, lifejacket, null];
+        }
+    }
+}
